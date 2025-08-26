41c9bbe08d2356b34e166e3813004f2dafb911ce
jrodrigo887 <drumsrodrigo@gmail.com>
2025-08-26 19:02:06 -0300

    feat: Implemente feature de hospedam de um hotel. - Métodos de Adiciona, calcular e obter quantidade ajustados.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,15 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            var Quantidade = hospedes.Count;
+            if (Quantidade <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentException($"Quantidade de hospedes excedeu o limite de {Suite.Capacidade} hospedes.");
             }
         }
 
@@ -36,25 +36,27 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
-        public decimal CalcularValorDiaria()
+        public string CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = valor - (valor * 0.1M);
             }
 
-            return valor;
+            return DateFormating(valor);
+        }
+
+        private string DateFormating(decimal value)
+        {
+            return value.ToString("c", new System.Globalization.CultureInfo("pt-BR"));
         }
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using System.Text;
 using DesafioProjetoHospedagem.Models;
+using Newtonsoft.Json;
 
 Console.OutputEncoding = Encoding.UTF8;
 
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+HospedagePessoa<Pessoa> hospedes = new HospedagePessoa<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
@@ -18,8 +19,65 @@ Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 // Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 5);
 reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+reserva.CadastrarHospedes(hospedes.GetValues);
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+Console.WriteLine(hospedes.ToString());
+
+
+public class HospedagePessoa<T>
+{
+
+  public HospedagePessoa()
+  {
+
+    Values = new List<T>();
+  }
+
+  private List<T> Values;
+
+  public void Add(T value)
+  {
+    FileAddObjet("SystemFile/hospedes.json", value);
+    Values.Add(value);
+  }
+
+  public List<T> GetValues => Values;
+  public override string ToString()
+  {
+    var obj = GetObjetJson("SystemFile/hospedes.json");
+    return $"Hospedes atuais: \n {obj}";
+  }
+  private void FileAddObjet(string pathAndFile, T newValue)
+  {
+    List<T> newObjList = new List<T>();
+
+    if (File.Exists(pathAndFile))
+    {
+      var objectJson = File.ReadAllText(pathAndFile);
+
+      List<T> values = JsonConvert.DeserializeObject<List<T>>(objectJson);
+      if (values != null)
+      {
+        newObjList.AddRange(values);
+      }
+
+    }
+
+    newObjList.Add(newValue);
+
+    var jsonConverted = JsonConvert.SerializeObject(newObjList, Formatting.Indented);
+
+    File.WriteAllText(pathAndFile, jsonConverted);
+
+  }
+
+  private string GetObjetJson(string pathName)
+  {
+    var objectJson = File.ReadAllText(pathName);
+    return objectJson;
+  }
+}
